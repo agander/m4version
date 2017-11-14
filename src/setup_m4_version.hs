735e05270536df7782b27f7c19b91c42de051202
gander <agander@gmail.com>
2017-11-14 23:25:53 +0100

    [0.0.2]: One function to print debug messages and a global Bool to test if m4/ exists.

--- src/setup_m4_version.hs
+++ src/setup_m4_version.hs
@@ -22,10 +22,32 @@ main = do
   putStr ">>> COMMENT: arg[0]: ["
   putStr prog
   putStrLn "]"
+  putNPrint "c" "inizio" prog
 
   args <- getArgs
   --nomeficheiro <- return( args !! 0)
   --putStrLn ( "Name is" ++ nomeficheiro)
   --if length args > 0 then putStrLn args else purStrLn Nothing
   mapM_ print args
+
+  -- | global indicator to be able to test/set if m4/exists.
+  haveM4 <- doesDirectoryExist "m4/"
+  putNPrint "c" "Do we have m4/?: " haveM4
+
+-- | Functions
+-- | Log message function
+--putNPrint :: Show a => Char -> String -> a -> IO ()
+putNPrint tipe msg var = do
+  case tipe of
+      "c" -> putStr ">>> COMMENT: "
+      "C" -> putStr ">>> COMMENT: "
+      "w" -> putStr "!!! WARNING: "
+      "W" -> putStr "!!! WARNING: "
+      "e" -> putStr "*** ERROR:   "
+      "E" -> putStr "*** ERROR:   "
+      _   -> putStr ">>> INFO:    "
+  putStr msg
+  putStr " "
+  print var
+
 
