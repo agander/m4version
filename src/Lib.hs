1eee1996d4e1dd189850c45efc2624606c459fcd
gander <agander@gmail.com>
2017-11-16 18:03:05 +0100

    [0.0.3]: WIP: i) about to install hpack-convert which will trash my cabal file; ii) work on function to process args into a tuple (arg, value).

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
-    ) where
+( someFunc
+) where
+
+import qualified Data.Text as T
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+argsToTuple :: [T.Text] -> [(T.Text, T.Text)]
+argsToTuple  = undefine
+--argsToTuple x:xs = [(x : argsToTuple xs)]
+--argsToTuple [] = [()]
+
--- src/setup_m4_version.hs
+++ src/setup_m4_version.hs
@@ -8,12 +8,13 @@ import System.Directory (listDirectory, doesDirectoryExist)
 --import Data.List.Split (splitOn)
 import System.FilePath (splitFileName, (</>)) -- searchPathSeparator)
 import Control.Monad (forM, mapM, forM_, mapM_)
+import Data.Text
 import qualified Data.Text as T
 
 import Lib
 
 --tokenise x y = h : if null t then [] else tokenise x (drop (length x) t)
---    where (h,t) = B.breakSubstring x y
+--  where (h,t) = B.breakSubstring x y
 
 main :: IO ()
 main = do
@@ -28,9 +29,12 @@ main = do
   --nomeficheiro <- return( args !! 0)
   --putStrLn ( "Name is" ++ nomeficheiro)
   --if length args > 0 then putStrLn args else purStrLn Nothing
+  -- | Convert args to Text
+  let tArgs = map T.pack args
   mapM_ print args
 
-  -- | global indicator to be able to test/set if m4/exists.
+  -- | global indicator to check if directory m4/ exists.
+  -- Use after to be able to check/create if it doesnt exist.
   haveM4 <- doesDirectoryExist "m4/"
   putNPrint "c" "Do we have m4/?: " haveM4
 
@@ -49,5 +53,12 @@ putNPrint tipe msg var = do
   putStr msg
   putStr " "
   print var
+
+
+-- | Setup a m4 version file with:
+-- define([VERSION_NUMBER],[0.0.3])dnl
+-- where the number is an arg.
+--printVersion :: 
+--printVersion = 
 
 
